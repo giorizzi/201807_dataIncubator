f0043b7a300a1d848937b53681d0a9d727eef791
Giovanni Rizzi <giovannirizzi@Giovannis-MacBook-Air.local>
2018-07-21 19:10:15 -0700

    <commit message>

--- Challenge3.py
+++ Challenge3.py
@@ -15,16 +15,16 @@ dir_queries = os.path.join(dir_script, './results/queries')
 if not os.path.exists(dir_queries):
     os.makedirs(dir_queries)
 #%%    
-client = ot.get_client()
-query_str = 'colorectal AND cancer'
+client = ot.get_client() #setup Open Trial cliant API
+query_str = 'colorectal AND cancer' #define search string 
 print('query_str:'+ query_str)
-results = ot.query(client, q=query_str, endpoint='trials')
+results = ot.query(client, q=query_str, endpoint='trials') # the query is used to search in trials, use ot_helpers to get all data
 print('{} {}'.format(results['total_count'], len(results['items'])))
 #pprint(results['items'][:1])
-f_pkl = os.path.join(dir_queries, '{}.pkl'.format(query_str))
+f_pkl = os.path.join(dir_queries, '{}.pkl'.format(query_str)) #save data to avoid excessive use of API
 ot.save_results(filename=f_pkl, results=results)
-#%%
-f_pkl = os.path.join(dir_queries, '{}.pkl'.format(query_str))
+#%% Reduces the data to only countries and year
+f_pkl = os.path.join(dir_queries, '{}.pkl'.format(query_str)) #open data
 results = ot.load_results(filename=f_pkl)
 locations=[]
 years=[]
@@ -32,23 +32,23 @@ for item in results['items']:
     #print(item['locations'])
     if item['locations'] and item['registration_date']:
        #locations.append([this['name'] for this in item['locations']])
-       locations.append(item['locations'][0]['name'])
+       locations.append(item['locations'][0]['name']) #only first country is picked
        years.append(item['registration_date'].year)
-#%%
+#%% count appearences of different countries in each year using pandas 
 locations=pd.Series(locations)
 years=pd.Series(years)
 
 trials=pd.DataFrame({'Registration year':years, 'Countries':locations})   
-trials.hist('Registration year')
-dummy=pd.get_dummies(trials['Countries'])
+trials.hist('Registration year') #initial look at data
+dummy=pd.get_dummies(trials['Countries']) #transform into dummy for efficient  counting
 trials=pd.concat([trials['Registration year'],dummy],axis=1)
-trials['count']=1
-by_country=trials.groupby('Registration year').sum()
+trials['count']=1 #in the next step this will generate a count per year
+by_country=trials.groupby('Registration year').sum() #group by year and sum appearences
 desc=by_country.describe()
 mean=-desc.ix[1]
-mean=mean.sort_values()
+mean=mean.sort_values()#find th emost represented countries
 #%%
-countries_toPlot=mean.index[1:7]
+countries_toPlot=mean.index[1:7] #select 6 most common countries and plot the ratio of total tries per country
 plt.figure(figsize=(13, 8))
   # Output a graph of loss metrics over periods.
 plt.subplot(2, 1, 2)
@@ -67,3 +67,4 @@ plt.title("Colorectal cancer trials per year")
 plt.tight_layout()
 plt.plot(by_country['count'])
 plt.xticks(np.arange(1999, 2018, 1.0))
+plt.savefig('plot1.pdf', bbox_inches='tight')
